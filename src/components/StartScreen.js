e31b9b5bd7b3cef67c1db3b0827e5f8d13b86436
Jenna Godfrey <jgodfrey@Jennas-MacBook-Pro.local>
2024-06-04 18:20:07 -0500

    adding navigation with useNavigation

--- src/components/StartScreen.js
+++ src/components/StartScreen.js
@@ -1,16 +1,17 @@
 // import logo from './logo.svg';
+import { useNavigate } from 'react-router-dom'
 import '../App.css';
 
 
 function StartScreen() {
-    // const history = useHistory();
+    const navigate = useNavigate();
     return (
         <div className="start-screen d-flex flex-column align-items-center justify-content-center">
             <p className="mb-4">
                 Welcome to a Little Mac remake.
             </p>
             <button className='btn btn-secondary mt-4'
-            onClick={console.log('clicked!')}>
+            onClick={() => navigate('/screening')}>
                 Start your journey
             </button>
         </div>
